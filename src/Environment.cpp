0e35bd573b43cb6f5e406675bbd01c7d55260902
Orestis Melkonian <sdi1000128@di.uoa.gr>
2015-01-13 19:35:14 +0200

    Environment is ok.

--- src/Environment.cpp
+++ src/Environment.cpp
@@ -6,6 +6,7 @@ Environment::Environment() : ObjectModel("resources/environment.obj","resources/
 	this->position = { 5, 5, -10 };
 	this->scale = { 500, 500, 500 };
 	this->rposition = { 0, 180, 0 };
+	this->rspeed = { 0.0001f, 0.001f, -0.001f };
 }
 
 
@@ -17,7 +18,7 @@ Environment::~Environment()
 void Environment::draw() {
 	glPushMatrix();
 	glDisable(GL_LIGHTING);
-	glTranslatef(0, 0, -500);
+	glTranslatef(0, 0, 10);
 	glColor3f(0.6, 0.6, 0.6);
 	ObjectModel::draw();
 	glEnable(GL_LIGHTING);
